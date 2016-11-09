485ea10c423e1dc7d0456a6519b56f91137febf3
Paul Brady <pablo.brady@gmail.com>
2016-11-09 15:04:34 -0800

    [Add]  Add initial tests.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-	response.send('ok');
+	response.send('OK');
 });
 
-app.listen(3000);
+module.exports = app;
